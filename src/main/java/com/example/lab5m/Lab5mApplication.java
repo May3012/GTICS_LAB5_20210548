a2304580c014f0c1761e8275ea944f7ef8398570
May3012 <139161004+May3012@users.noreply.github.com>
2024-05-09 19:17:56 -0500

    primer commit

---
+++ src/main/java/com/example/lab5m/Lab5mApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab5m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5mApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5mApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab5m/Lab5mApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5mApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
